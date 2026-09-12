9fd296d73a47714103a1e7884c671c3c164f17b5
Mahmoud Ashraf <mahmoud.ashraf4548@gmail.com>
2026-09-12 16:42:04 +0300

    Fix mobile app installation behavior

--- src/components/AdLayoutWrapper.tsx
+++ src/components/AdLayoutWrapper.tsx
@@ -15,9 +15,9 @@ export default function AdLayoutWrapper({ children, leftAds, rightAds }: AdLayou
         
         {/* Left Ad Rail */}
         {leftAds.length > 0 && (
-          <aside className="flex w-[45px] sm:w-[80px] md:w-[120px] lg:w-[160px] xl:w-[200px] 2xl:w-[300px] shrink-0 flex-col gap-2 lg:gap-4 p-1 sm:p-2 lg:p-4 sticky top-[73px] h-[calc(100vh-73px)] overflow-y-auto overflow-x-hidden no-scrollbar">
+          <aside className="flex w-[clamp(65px,22vw,140px)] lg:w-[160px] xl:w-[200px] 2xl:w-[300px] shrink-0 flex-col gap-2 lg:gap-4 p-1 sm:p-2 lg:p-4 sticky top-[73px] h-[calc(100vh-73px)] overflow-y-auto overflow-x-hidden no-scrollbar">
             {leftAds.map(ad => (
-              <Advertisement key={ad.id} ad={ad} className="w-full flex-1 rounded-sm lg:rounded-lg" mediaClassName="w-full h-full object-contain lg:object-cover" />
+              <Advertisement key={ad.id} ad={ad} className="w-full lg:flex-1 rounded-sm lg:rounded-lg overflow-hidden" mediaClassName="w-full h-auto lg:h-full object-contain lg:object-cover" />
             ))}
           </aside>
         )}
@@ -29,9 +29,9 @@ export default function AdLayoutWrapper({ children, leftAds, rightAds }: AdLayou
 
         {/* Right Ad Rail */}
         {rightAds.length > 0 && (
-          <aside className="flex w-[45px] sm:w-[80px] md:w-[120px] lg:w-[160px] xl:w-[200px] 2xl:w-[300px] shrink-0 flex-col gap-2 lg:gap-4 p-1 sm:p-2 lg:p-4 sticky top-[73px] h-[calc(100vh-73px)] overflow-y-auto overflow-x-hidden no-scrollbar">
+          <aside className="flex w-[clamp(65px,22vw,140px)] lg:w-[160px] xl:w-[200px] 2xl:w-[300px] shrink-0 flex-col gap-2 lg:gap-4 p-1 sm:p-2 lg:p-4 sticky top-[73px] h-[calc(100vh-73px)] overflow-y-auto overflow-x-hidden no-scrollbar">
             {rightAds.map(ad => (
-              <Advertisement key={ad.id} ad={ad} className="w-full flex-1 rounded-sm lg:rounded-lg" mediaClassName="w-full h-full object-contain lg:object-cover" />
+              <Advertisement key={ad.id} ad={ad} className="w-full lg:flex-1 rounded-sm lg:rounded-lg overflow-hidden" mediaClassName="w-full h-auto lg:h-full object-contain lg:object-cover" />
             ))}
           </aside>
         )}
